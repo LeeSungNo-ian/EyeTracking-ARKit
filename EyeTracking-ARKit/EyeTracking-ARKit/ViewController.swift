2b4043280d787fcd5701440ddd19bd6b4152b7a4
LeeSungNo-ian <isnn0103@gmail.com>
2022-08-30 02:10:14 +0900

    feat: Session Configuration 생성
    - 현 디바이스 ARKit 실행 가능 여부 조사
    - ARFaceTrackingConfiguration 생성

--- EyeTracking-ARKit/EyeTracking-ARKit/ViewController.swift
+++ EyeTracking-ARKit/EyeTracking-ARKit/ViewController.swift
@@ -23,6 +23,14 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        guard ARFaceTrackingConfiguration.isSupported else {
+            print("지원하지 않는 디바이스 입니다.")
+            return
+        }
+        
+        let configuration = ARFaceTrackingConfiguration()
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     override func viewWillDisappear(_ animated: Bool) {
