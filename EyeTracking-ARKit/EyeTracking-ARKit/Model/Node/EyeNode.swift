1fb78a95fa46104e6f78f36318523b82475e33e6
LeeSungNo-ian <isnn0103@gmail.com>
2022-11-07 17:43:38 +0900

    refactor: class EyeNode 로직 개선

--- EyeTracking-ARKit/EyeTracking-ARKit/Model/Node/EyeNode.swift
+++ EyeTracking-ARKit/EyeTracking-ARKit/Model/Node/EyeNode.swift
@@ -14,21 +14,29 @@ final class EyeNode: SCNNode {
     init(color: UIColor) {
         super.init()
 
+        configureNode(color: color)
+        configureTarget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureNode(color: UIColor) {
         let cylinder = SCNCylinder(radius: 0.01, height: 0.15)
         cylinder.firstMaterial?.diffuse.contents = color
-
+        
         let node = SCNNode(geometry: cylinder)
         node.eulerAngles.x = .pi/2
         node.position.z = 0.075
         node.opacity = 0.4
 
-        target.position.z = 1
-
         addChildNode(node)
-        addChildNode(target)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func configureTarget() {
+        target.position.z = 1
+        
+        addChildNode(target)
     }
 }
